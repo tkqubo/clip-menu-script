6e1d8cc7c831de907b1830568005b4acf9fd8776
tkqubo <tk.qubo@gmail.com>
2015-11-04 20:05:25 +0900

    fix: separator

--- action/format-markdown-table.js
+++ action/format-markdown-table.js
@@ -1,12 +1,37 @@
 var isSeparator = /^(:?)(-+)(:?)$/;
 
+function trimCell(text) {
+  text = text.trim();
+  var separator = text.match(isSeparator);
+  if (separator) {
+    text = separator[1] + "-" + separator[3];
+  }
+  return text;
+}
+
+function pad(text, length) {
+  var separator = text.match(isSeparator);
+  if (separator) {
+    text = separator[2];
+    while ((separator[1] + text + separator[3]).length < length + 2) {
+      text += "-";
+    }
+    return separator[1] + text + separator[3];
+  } else {
+    while (text.length < length) {
+      text += " ";
+    }
+    return " " + text + " ";
+  }
+}
+
 function parseToTable(text) {
   var table = text
     .split("\n")
     .map(function (line) {
       return line
         .split("|")
-        .map(function (cell) { return cell.trim(); });
+        .map(trimCell);
     });
   var maxTextLengthPerColumn = [];
   for (var i = 0; i < table[0].length; i++) {
@@ -27,22 +52,6 @@ function formatToString(table) {
   return table
     .map(function (row) { return row.join("|").trim(); })
     .join("\n");
-}
-
-function pad(text, length) {
-  var separator = text.match(isSeparator);
-  if (separator) {
-    text = separator[2];
-    while ((separator[1] + text + separator[3]).length < length + 2) {
-      text += "-";
-    }
-    return separator[1] + text + separator[3];
-  } else {
-    while (text.length < length) {
-      text += " ";
-    }
-    return " " + text + " ";
-  }
 }
 
 var table = parseToTable(clipText);
